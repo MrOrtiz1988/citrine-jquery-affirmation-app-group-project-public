39c53df90242f3d0c1cdd88d8522d530bbe04ad8
Luis Ortiz <l.ortizintec@gmail.com>
2023-03-31 15:51:54 -0400

    complete

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 $(document).ready(onReady);
 
 function onReady() {
-  console.log('We got this.')
+  $( '#fire-button' ).on( 'click', addFire);
+  $( '#ice-button' ).on( 'click', addIce);
+  console.log('We got this.');
+  $('.submit-button').on('click', addAffirmation);
+  $('tbody').on('click', '#delete-button', deleteThis);
+
+}
+function deleteThis(){
+  if($('.fire').length < $('.ice').length){
+    $(this).parent().parent().remove();
+  }
+}
+
+function addFire() {
+  // NOTE: will add class to span later.
+  $( '#fire-ice-container' ).append( '<span>🔥</span>');
+}
+
+function addIce() {
+  // NOTE: will add class to span later.
+  $( '#fire-ice-container' ).append( '<span>❄️</span>' );
+}
+
+function addAffirmation(event){
+  event.preventDefault();
+  let readAffirmation = $('#affirmation-input').val();
+  let readAuthor = $('#author-input').val();
+  if($('.fire').length > $('.ice').length){
+    $('tbody').append(`
+    <tr id="table-row">
+    <td>${readAffirmation}</td>
+    <td>${readAuthor}</td>
+    <td><button id="delete-button">❌</button></td>
+    </tr>
+    `);
+  $('#affirmation-input').val("");
+  $('#author-input').val("");
+
+  }
 }
